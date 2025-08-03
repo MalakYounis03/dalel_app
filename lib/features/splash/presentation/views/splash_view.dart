07ce937447b15e13bb76a346be739ba2f14112fd
MalakYounis03 <ibrhr55@gmail.com>
2025-08-03 11:27:41 +0300

    splash view

--- lib/features/splash/presentation/views/splash_view.dart
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -1,4 +1,6 @@
 import 'package:dalel_app/core/functions/navigation.dart';
+import 'package:dalel_app/core/utils/app_strings.dart';
+import 'package:dalel_app/core/utils/app_text_styles.dart';
 import 'package:flutter/material.dart';
 
 class SplashView extends StatefulWidget {
@@ -11,14 +13,25 @@ class SplashView extends StatefulWidget {
 class _SplashViewState extends State<SplashView> {
   @override
   void initState() {
-    Future.delayed(Duration(seconds: 2), () {
+    delayedNavigate();
+    super.initState();
+  }
+
+  Future<Null> delayedNavigate() {
+    return Future.delayed(Duration(seconds: 2), () {
       customNavigate(context, "/onBoarding");
     });
-    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Text(
+          AppStrings.appName,
+          style: CustomTextStyles.pacifico400style64,
+        ),
+      ),
+    );
   }
 }
